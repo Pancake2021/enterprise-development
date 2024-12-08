7af9abd79774399ad3a2dad9a7ff6988de74f071
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2024-12-08 10:45:55 +0400

    fix: переименование папки и файла проекта API
    - Удалена папка UniversityStats.Api
    - Создана папка UniversityStats.API
    - Переименован файл проекта в UniversityStats.API.csproj

--- UniversityStats/UniversityStats.API/obj/Debug/net8.0/UniversityStats.API.AssemblyInfo.cs
+++ UniversityStats/UniversityStats.API/obj/Debug/net8.0/UniversityStats.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityStats.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c8cff5087503f80b9a8c12cf67d121db4c7263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cf13c28d678c6dcbb4d1daab05dfff6f42e66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityStats.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityStats.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
